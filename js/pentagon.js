6bddfd0b741a1910c0a288a3929ce6a577790546
RabeyaAkter78 <rabeyariya251@gmail.com>
2023-02-18 20:22:48 +0600

    added pentagon

---
+++ js/pentagon.js
@@ -0,0 +1,30 @@
+
+document.getElementById('pentagon-btn').addEventListener('click', function () {
+    serial += 1;
+
+    const geometryName = document.getElementById('name-pentagon').innerText;
+
+    // get the value of Area:
+    const areaOfGeometry = document.getElementById('pentagon-area').innerText;
+
+    // get the Base and Width of triangle:
+    const WidthOfGeometry = document.getElementById('pentagon-p');
+    const newWidthOfGeometryString = WidthOfGeometry.value;
+    newWidthOfGeometry = parseFloat(newWidthOfGeometryString);
+
+    const BaseOfGeometry = document.getElementById('pentagon-b');
+    const newBaseOfGeometryString = BaseOfGeometry.value;
+    newBaseOfGeometry = parseFloat(newBaseOfGeometryString);
+
+    // calculate the area:
+    const newAreaOfGeometry = 0.5*(newWidthOfGeometry * newBaseOfGeometry);
+    // newAreaOfGeometry = areaOfGeometry;
+
+    // validation:
+    if (isNaN(newAreaOfGeometry)) {
+        alert('Please enter a valid Number');
+        return;
+    }
+
+    displayData(geometryName, newAreaOfGeometry);
+});
