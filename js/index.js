614ae62165780ba8238e1c2552c6f074c2413809
RabeyaAkter78 <rabeyariya251@gmail.com>
2023-02-18 18:27:35 +0600

    added rectangle card

--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
-console.log('hello world')
+//  for triangle:
+// set addEventListener on btn:
+let serial=0;
+document.getElementById('triangle-btn').addEventListener('click', function () {
+
+    serial+=1;
+    const geometryName = document.getElementById('name-triangle').innerText;
+
+    // get the value of Area:
+    const areaOfGeometry = document.getElementById('triangle-area').innerText;
+
+    // get the height and base of triangle:
+    const baseOfGeometry = document.getElementById('triangle-base');
+    const newBaseOfGeometryString = baseOfGeometry.value;
+    newBaseOfGeometry = parseFloat(newBaseOfGeometryString);
+
+    const heightOfGeometry = document.getElementById('triangle-height');
+    const newHeightOfGeometryString = heightOfGeometry.value;
+    newHeightOfGeometry = parseFloat(newHeightOfGeometryString);
+
+    // calculate the area:
+    const newAreaOfGeometry =(newBaseOfGeometry * newHeightOfGeometry);
+    anewAreaOfGeometry = areaOfGeometry;
+
+    // // clear the inout area:
+    // baseOfGeometry = '';
+    // heightOfGeometry = '';
+
+    // console.log(geometryName, newBaseOfGeometry, newHeightOfGeometry, newAreaOfGeometry);
+
+    displayData(geometryName,newAreaOfGeometry);
+});
+
+function displayData (geometryName,newAreaOfGeometry){
+    const container = document.getElementById("table-container");
+
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${serial}</td>
+    <td>${geometryName}</td>
+    <td>${newAreaOfGeometry}</td>
+    `;
+    
+    container.appendChild(tr);
+    
+
+}
---
+++ js/rectangle.js
@@ -0,0 +1,25 @@
+
+document.getElementById('rectangle-btn').addEventListener('click', function () {
+    serial+=1;
+
+    const geometryName = document.getElementById('name-rectangle').innerText;
+
+    // get the value of Area:
+    const areaOfGeometry = document.getElementById('rectangle-area').innerText;
+
+    // get the Length and Width of triangle:
+    const WidthOfGeometry = document.getElementById('rectangle-w');
+    const newWidthOfGeometryString = WidthOfGeometry.value;
+    newWidthOfGeometry = parseFloat(newWidthOfGeometryString);
+
+    const LengthOfGeometry = document.getElementById('rectangle-l');
+    const newLengthOfGeometryString = LengthOfGeometry.value;
+    newLengthOfGeometry = parseFloat(newLengthOfGeometryString);
+
+    // calculate the area:
+    const newAreaOfGeometry =(newWidthOfGeometry * newLengthOfGeometry);
+    anewAreaOfGeometry = areaOfGeometry;
+
+
+    displayData(geometryName,newAreaOfGeometry);
+});
